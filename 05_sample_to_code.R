5a70760a2fc991b57b0bfe62e243340c5c590015
Max Dugan-Knight <maxduganknight@gmail.com>
2021-06-09 12:17:52 +0100

    improvements to classifier performance assessment

--- 05_sample_to_code.R
+++ 05_sample_to_code.R
@@ -9,12 +9,20 @@ library(dplyr)
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 speeches <- readRDS("data/covid_speeches.rds")
-covid_keywords <- c("covid", "coronavirus", "pandemic", "lockdown", "pandemic", "epidemic", "covid-19")
+covid_keywords <- c("covid", "coronavirus", "lockdown", "pandemic", 
+                    "epidemic", "covid-19")
+
+# covid_keywords_tier <- c("covid", "coronavirus", "lockdown", "pandemic", 
+#                     "epidemic", "covid-19", "tier")
 
 corpus <- corpus(speeches, text_field = "speech")
 
 covid_corpus <-  corpus_subset(corpus, grepl(paste(covid_keywords,collapse = "|"),
-                                             texts(corpus))) 
+                                             as.character(corpus))) 
+
+# covid_corpus_tier <-  corpus_subset(corpus, grepl(paste(covid_keywords_tier, 
+#                                                         collapse = "|"), 
+#                                                   as.character(corpus))) 
 
 # samples for manual classification
 # create first sample
@@ -28,4 +36,15 @@ covid_sample_idx_2 <- sample(seq(1:length(covid_corpus)), size = 300) %>%
 covid_sample_2 <- covid_corpus[covid_sample_idx_2]
 texts_2 <- cbind(texts(covid_sample_2), docvars(covid_corpus[covid_sample_idx_2]))
 
-write.csv(texts_2, "data/sample_texts2.csv")
+# I added the keyword "tier" part way through my manual coding so I'm going to
+# now code the approximate percentage of texts that adding "tier" to the list adds
+# to the number of texts in the corpus. 
+
+tier_corpus <-  corpus_subset(corpus, grepl(paste("tier",collapse = "|"),
+                                             as.character(corpus))) 
+
+tier_sample_idx <- sample(seq(1:length(tier_corpus)), size = 18)
+tier_sample <- tier_corpus[tier_sample_idx]
+tier_texts <- cbind(as.character(tier_sample), docvars(tier_corpus[tier_sample_idx]))
+
+write.csv(tier_texts, "data/sample_tier_texts.csv")
--- 06_classification.R
+++ 06_classification.R
@@ -9,19 +9,35 @@ library(glmnet)
 library(doMC)
 library(ranger)
 
-
-
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
-# function to compute performance metrics
-precrecall <- function(mytable, classes=2) {
-  true_positives <- mytable[1,1]
-  false_positives <- sum(mytable[1,]) - true_positives
-  false_negatives <- sum(mytable[,1]) - true_positives
-  precision <- true_positives / (true_positives + false_positives)
+## Functions
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# function to compute performance metrics for 3 class classifier
+get_acc_F1 <- function(mytable) {
+  class_1_true_positives <- mytable[1,1]
+  class_2_true_positives <- mytable[2,2]
+  class_1_false_positives <- sum(mytable[1,]) - class_1_true_positives
+  class_2_false_positives <- sum(mytable[2,]) - class_2_true_positives
+  # precision
+  class_1_precision <- class_1_true_positives / 
+    (class_1_true_positives + class_1_false_positives)
+  class_2_precision <- class_2_true_positives / 
+    (class_2_true_positives + class_2_false_positives)
+  # recall
+  class_1_recall <- class_1_true_positives / 
+    (sum(mytable[,1]))
+  class_2_recall <- class_2_true_positives / 
+    (sum(mytable[,2]))
+  # accuracy
   accuracy <- sum(diag(mytable)) / sum(mytable)
-  recall <- true_positives / (true_positives + false_negatives)
-  output <- list(precision, recall, accuracy)
+  # F1
+  class_1_F1 <- 2*(class_1_recall * class_1_precision) / 
+    (class_1_recall + class_1_precision)
+  class_2_F1 <- 2*(class_2_recall * class_2_precision) / 
+    (class_2_recall + class_2_precision)
+  output <- list(accuracy, class_1_F1, class_2_F1)
   return(output)
 }
 
@@ -35,10 +51,13 @@ get_posterior <- function(nb) {
   PwGc
 }
 
+## Text Processing
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 speeches <- readRDS("data/covid_speeches.rds")
 addtl_stopwords <- c("hon", "hon_friend", "government", "people")
 covid_keywords <- c("covid", "coronavirus", "pandemic", "lockdown", "pandemic", 
-                    "epidemic", "covid-19")
+                    "epidemic", "covid-19", "tier")
 corpus <- corpus(speeches, text_field = "speech")
 covid_corpus <-  corpus_subset(corpus, grepl(paste(covid_keywords,collapse = "|"),
                                                    texts(corpus))) 
@@ -47,6 +66,7 @@ covid_corpus <-  corpus_subset(corpus, grepl(paste(covid_keywords,collapse = "|"
 # create dfm
 dfm <- covid_corpus %>%
   tokens(remove_punct = TRUE, remove_numbers = TRUE, remove_symbols = TRUE) %>%
+  tokens_tolower() %>%
   #tokens_select(min_nchar = 2) %>%
   #tokens_ngrams(n = 1:2) %>% # up to bigrams
   # for some reason there were a lot of words connected by .
@@ -62,9 +82,12 @@ dfm
 dfm <- dfm[,sort(featnames(dfm))]
 
 # wordcloud
-library("quanteda.textplots")
-textplot_wordcloud(dfm, min_count = 10, random_order = FALSE, rotation = 0.25, 
-                   color = RColorBrewer::brewer.pal(8, "Dark2"))
+# library("quanteda.textplots")
+# textplot_wordcloud(dfm, min_count = 10, random_order = FALSE, rotation = 0.25, 
+#                    color = RColorBrewer::brewer.pal(8, "Dark2"))
+
+## Hand-classified Texts
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # read in hand-classified texts
 classified_texts <- read.csv("data/sample_texts.csv", header = TRUE) %>%
@@ -82,7 +105,7 @@ classified_texts_balanced <- rbind(
       unknown_size, downsample_size),]
   ) 
 
-colnames(classified_texts) <- c("textid", "text", "person_id", "first_name", 
+colnames(classified_texts_balanced) <- c("textid", "text", "person_id", "first_name", 
                                 "last_name", "date", "party", "constituency", 
                                 "label")
 
@@ -92,11 +115,13 @@ rownames(labelled) <- classified_texts_balanced[,1]
 # order by row names
 labelled <- labelled[ order(row.names(labelled)), ]
 
+
+## Naive Bayes
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # cross validation on training data
 # randomly shuffle training data before splitting
 set.seed(3)
-# df_train <- classified_texts_balanced[sample(nrow(classified_texts_balanced)), 
-#                              c("textid", "label")]
 
 df_train <- labelled[sample(nrow(labelled)),] %>%
   select(label)
@@ -109,8 +134,8 @@ splits <- split(df_train, rep(1:ceiling(nr/n),
                               each = n, length.out = nr)) 
 
 # use splits to train nb model on 4 out of 5 splits and test on 5th.
-precision <- numeric(k)
-recall <- numeric(k)
+class_1_F1 <- numeric(k)
+class_2_F1 <- numeric(k)
 accuracy <- numeric(k)
 tables <- vector(mode = "list", length = k)
 
@@ -121,17 +146,21 @@ for (i in 1:length(splits)) {
   train_y <- train$label
   test_idx <- rownames(splits[[i]])
   test_y <- splits[[i]]$label
-  nb <- textmodel_nb(dfm[train_idx,], train_y)
+  nb <- textmodel_nb(dfm[train_idx,], train_y, 
+                     distribution = "multinomial", prior = "termfreq")
   preds <- predict(nb, newdata = dfm[test_idx,])
   cm <- table(preds, test_y)
   tables[[i]] <- cm
-  precision[i] <- precrecall(cm)[[1]]
-  recall[i] <- precrecall(cm)[[2]]
-  accuracy[i] <- precrecall(cm)[[3]]
+  accuracy[i] <- get_acc_F1(cm)[[1]]
+  class_1_F1[i] <- get_acc_F1(cm)[[2]]
+  class_2_F1[i] <- get_acc_F1(cm)[[3]]
 }
-print(mean(precision))
-print(mean(recall))
+print(mean(class_1_F1))
+print(mean(class_2_F1))
 print(mean(accuracy))
+
+## Regularized Regressions
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # training and test sets
 test_fraction <- 0.3
@@ -148,29 +177,31 @@ test_y <- labelled[testing_indices, "label"]
 
 # ridge regression
 registerDoMC(cores=8) # adjust
-ridge_model <- cv.glmnet(training_X, training_y, alpha=0, nfolds=5, 
+ridge_model <- cv.glmnet(training_X, training_y, alpha=0, 
                       family = "multinomial", parallel=TRUE)
 plot(ridge_model)
 
 # multinomial lasso classifier
 lasso_model <- cv.glmnet(training_X, training_y, 
-                         family="multinomial", alpha=1, nfolds=5, 
+                         family="multinomial", alpha=1, 
                          parallel=TRUE, intercept=TRUE, standardize = TRUE)
 plot(lasso_model)
 
 # evaluation
 # Prediction
-test_y_hat <- predict(lasso, test_X, type="class")
-
-# Accuracy
-sum(test_y_hat == test_y)/length(test_y)
+test_y_hat <- predict(lasso_model, test_X, type="class")
 
 # Confusion matrix
 table(test_y_hat, test_y)
 
+accuracy <- get_acc_F1(table(test_y_hat, test_y))[[1]]
+class_1_F1 <- get_acc_F1(table(test_y_hat, test_y))[[2]]
+class_2_F1 <- get_acc_F1(table(test_y_hat, test_y))[[3]]
+
 
 # random forest
-rf_model <- ranger(x = training_X, y = factor(training_y))
+rf_model <- ranger(x = training_X, y = factor(training_y), 
+                   importance = "impurity")
 test_y_hat <- predict(rf_model, test_X)$predictions
 
 # Accuracy
@@ -180,15 +211,12 @@ sum(test_y_hat == test_y)/length(test_y)
 table(test_y_hat, test_y)
 
 #feature importance
-head(importance(rf_model)) %>% sort(decreasing = TRUE) %>% head(20)
-
-
+importance(rf_model) %>% sort(decreasing = TRUE) %>% head(20)
 
 # performance metrics
-precrecall(cm) # precision, recall
-sum(diag(cm)) / sum(cm) # accuracy
-
-
+accuracy <- get_acc_F1(table(test_y_hat, test_y))[[1]]
+class_1_F1 <- get_acc_F1(table(test_y_hat, test_y))[[2]]
+class_2_F1 <- get_acc_F1(table(test_y_hat, test_y))[[3]]
 
 
 train_rows <- dfm[which(rownames(dfm) %in% df_train$textid),]
